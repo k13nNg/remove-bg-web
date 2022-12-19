b74e9eca0c6b34140b8f9a71af685fd0680c62e9
Michael Xie <michaelx.10@hotmail.com>
2022-12-18 20:49:53 -0500

    Landing Page First Draft
    Finished Landing page, look for any errors as needed

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+# URLConf
 urlpatterns = [
-    path('upload_image/', views.upload_image_render)
+    path('upload_image/', views.upload_image_render),
+    path('index/', views.main_render)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# view functions are functions that takes a request and 
+# view functions are functions that takes a request and
 # returns a response
 
 # view functions are also called request handler
+
+
+def main_render(request):
+    return render(request, 'index.html')
+
 
 def upload_image_render(request):
     return render(request, 'upload_image.html')
