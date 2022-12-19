eb57896cb050bb1ac2cd9a7d21da92c20284aaff
K12573 <75595156+K12573@users.noreply.github.com>
2022-12-19 15:16:29 -0500

    Able to display uploaded image

--- webapp/static/js/upload_image.js
+++ webapp/static/js/upload_image.js
@@ -6,17 +6,34 @@ var uploaded_image = "";
 
 real_file_reader_Btn.addEventListener("change", function(){
     const reader = new FileReader();
+
     reader.addEventListener("load", () => {
-        uploaded_image = reader.result;
-        const ctx = document.getElementById("display-image");
-        document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+        var img = new Image(),
+        canvas = document.getElementById("display-image"),
+        ctx = canvas.getContext("2d");
+
+        img.src = reader.result;
+        // document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+        
+
+        console.log("img.width: ", this.width);
+        console.log("img.height: ", this.height);
+        img.onload = function(){
+            let width = img.width;
+            let height = img.height;
+
+            canvas.width=width;
+            canvas.height=height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+        }
         
     });
+
     reader.readAsDataURL(this.files[0]);
+    
+    
 })
 
 customBtn.addEventListener("click", function() {
     real_file_reader_Btn.click();
 });
-
-console.log (customBtn)
