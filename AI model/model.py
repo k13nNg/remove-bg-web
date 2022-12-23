e771bfd42141726c538e4ad7e8ea110d9d3861c9
Tom Nguyen <tomnguyen@Toms-Air.hitronhub.home>
2022-12-22 21:30:54 -0500

    cosmetic change to upload_image page + fixed css not loading problem

--- AI model/model.py
+++ AI model/model.py
@@ -1,10 +1,14 @@
 from rembg import remove
 from PIL import Image
 
-input_path = 'AI model/test_images/selfie.jpeg'
-output_path = 'AI model/test_images/selfie-adjusted.png'
+input_path = 'AI model/test_images/dog.jpeg'
+output_path = 'AI model/test_images/dog-adjusted.png'
 
-input = Image.open(input_path)
-output=remove(input)
-output.save(output_path)
+# input = Image.open(input_path)
+# output=remove(input)
+# output.save(output_path)
+
+# Function used to remove background from image
+def remove_bg(input):
+    return remove(input)
 
--- webapp/static/js/upload_image.js
+++ webapp/static/js/upload_image.js
@@ -1,39 +1,45 @@
-const real_file_reader_Btn = document.getElementById("real-file-reader");
-const customBtn = document.getElementById("custom-button");
-//const image_input = document.querySelector("#real-file-reader");
+const input = document.getElementById("input");
+const uploadBtn = document.getElementById("upload-button");
 
-var uploaded_image = "";
-
-real_file_reader_Btn.addEventListener("change", function(){
-    const reader = new FileReader();
-
-    reader.addEventListener("load", () => {
-        var img = new Image(),
-        canvas = document.getElementById("display-image"),
-        ctx = canvas.getContext("2d");
-
-        img.src = reader.result;
-        // document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-        
-
-        console.log("img.width: ", this.width);
-        console.log("img.height: ", this.height);
-        img.onload = function(){
-            let width = img.width;
-            let height = img.height;
-
-            canvas.width=width;
-            canvas.height=height;
-            ctx.drawImage(img, 0, 0, img.width, img.height);
-        }
-        
-    });
-
-    reader.readAsDataURL(this.files[0]);
-    
-    
-})
-
-customBtn.addEventListener("click", function() {
-    real_file_reader_Btn.click();
+uploadBtn.addEventListener("click", function() {
+    input.click();
 });
+
+input.addEventListener("change", (event) => {
+    let image_file = event.target.files[0];
+    let reader = new FileReader();
+    const resize_width = 400;
+
+    reader.readAsDataURL(image_file);
+
+    // if there is already an image displayed, remove that image
+    if(document.getElementById("display-image").firstChild){
+        document.getElementById("display-image").innerHTML="";
+    }
+    
+    reader.onload = (event) => {
+        let image_url = event.target.result;
+
+        let image = document.createElement("img");
+        image.src = image_url;
+
+        image.onload = (e) => {
+            let canvas = document.createElement("canvas");
+            let ratio = resize_width / e.target.width;
+
+            canvas.width = resize_width;
+            canvas.height =  e.target.height * ratio;
+
+            const ctx = canvas.getContext("2d");
+
+            ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+            let new_image_url = ctx.canvas.toDataURL("image/jpeg", 90);
+
+            // display the uploaded image
+            let new_image = document.createElement("img");
+            new_image.src = new_image_url;
+            document.getElementById("display-image").appendChild(new_image);
+        }
+    }
+})
