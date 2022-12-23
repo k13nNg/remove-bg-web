bd8f7f2fc981a0112f746219886e49c326130348
Tom Nguyen <tomnguyen@Toms-Air.hitronhub.home>
2022-12-22 23:41:46 -0500

    Code structure change to upload_image, adding ajax call
    Code structure change to upload_image, adding ajax call

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('upload_image/', views.upload_image_render),
     path('index/', views.main_render, name="index"),  # default page
-    path('about/', views.about_render, name="about")
+    path('about/', views.about_render, name="about"),
+    # path('generate/', views.get_remove_bg_image, name = "generate_adjusted_image")
 ]
